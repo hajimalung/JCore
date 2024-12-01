25b75e25c93058fec908dee1939260fcf2df6b41
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-12-01 10:21:17 +0530

    initail commit

---
+++ DTypes.java
@@ -0,0 +1,50 @@
+public class DTypes {
+    public static void main(String[] args) {
+        // primitive datatypes
+
+        // boolean data tye
+        boolean bo = true;
+        System.out.println(bo);
+
+        // number data types
+
+        // allocates one byte memory
+        byte b = 12;
+        System.out.println(b);
+
+        // allocates 2 bytes of memory
+        short sh = 130;
+        System.out.println(sh);
+
+        // allocates 4 bytes of memory
+        // int is the default type for numbers in java
+        int i = 465465456;
+        System.out.println(i);
+        System.out.println(34534534);
+
+        // allocates 8 bytes of memory
+        long l = 465465456945l;
+        System.out.println(l);
+
+        // numbers with decimol point data types
+
+        // allocates 4 bytes of memory
+        float fl = 123.45f;
+        System.out.println(fl);
+
+        // allocates 8 bytes of memory
+        // double is the default data type for decimol point numbers in java
+        double db = 21344.45345;
+        System.out.println(db);
+
+        // textual data types
+        // allocates 2 bytes of memory
+        char c = 'a';
+        System.out.println(c);
+
+        // String is derived Datatype implemented by java
+        // it takes memomy based on the String length
+        String str = "This is a String!";
+        System.out.println(str);
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+// can be done in single line in jshell
+// jshell was introducd in java 9
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
